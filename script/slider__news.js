634bbbd5a4d59eff024e61d1c07043c1baa33209
LamVanHung2003 <111829358+LamVanHung2003@users.noreply.github.com>
2023-04-09 17:58:55 +0700

    Delete script directory

--- script/slider__news.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('next_news').onclick = function(){
-    const widthItem = document.querySelector('.item__news').offsetWidth;
-    document.getElementById('formList__news').scrollLeft += widthItem;
-}
-document.getElementById('prev_news').onclick = function(){
-    const widthItem = document.querySelector('.item__news').offsetWidth;
-    document.getElementById('formList__news').scrollLeft -= widthItem;
-}
-  
